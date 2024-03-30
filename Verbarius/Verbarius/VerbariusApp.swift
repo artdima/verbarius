ce34aea02e7dbbba54e47ae6d4f2e9cea1397d9d
Medyannik Dmitriy <mail@artdima.ru>
2024-03-30 11:26:43 +0400

    Init project

---
+++ Verbarius/Verbarius/VerbariusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerbariusApp.swift
+//  Verbarius
+//
+//  Created by Medyannik Dmitri on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct VerbariusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
